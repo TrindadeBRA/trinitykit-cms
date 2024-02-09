fa8195541d008033fb7349ec7b4578e9f4278632
Lucas Trindade <trindadebra@gmail.com>
2024-02-09 18:51:34 -0300

    footer hide

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
-    <footer id="footer" class="footer-dark bg-dark">
+    <!-- <footer id="footer" class="footer-dark bg-dark">
         <div class="site-info">
             <div class="container">
                 <p class="text-white text-center py-4">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> - <?php bloginfo('description'); ?>. Todos os direitos reservados.</p>
             </div>
         </div>
-    </footer>
+    </footer> -->
     <?php wp_footer(); ?>
 </body>
 </html>

eadf5619f3b87f1005eadc3a6d8958c71735c1af
Lucas Trindade <trindadebra@gmail.com>
2024-02-09 18:45:13 -0300

    Fix index from home

--- header.php
+++ header.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?></title>
     <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class("bg-gray-700"); ?>>
 
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
-                <img src="<?php echo get_theme_file_uri('assets/images/logo.webp'); ?>" alt="Logotipo">
+    <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container mx-auto">
+            <a class="navbar-brand flex items-center" href="<?php echo esc_url(home_url('/')); ?>">
+                <img src="<?php echo get_theme_file_uri('assets/images/logo.webp'); ?>" alt="Logotipo" class="mx-auto">
             </a>
         </div>
-    </nav>
+    </nav> -->
--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php get_header(); ?>
+<?php
+get_header(); // Adiciona o cabeçalho do tema 
+?>
 
-<?php wp_footer(); ?>
+<main class="container mx-auto h-full">
+    <div class="h-[100vh] w-full flex flex-col items-center justify-center">
+        <!-- Logotipo -->
+        <div class="my-4">
+            <a href="<?php echo esc_url(admin_url('/')); ?>">
+                <img src="<?php echo get_theme_file_uri('assets/images/logo.webp'); ?>" alt="Logotipo" class="mx-auto">
+            </a>
+        </div>
+    
+        <!-- Botão para o painel de administração -->
+        <a href="<?php echo esc_url(admin_url()); ?>" class="bg-black text-black px-4 py-2 rounded hover:bg-black-700 text-white font-bold">Painel Administrativo</a>
+    </div>
+</main>
+
+<?php
+get_footer(); // Adiciona o rodapé do tema
+wp_footer(); // Adiciona scripts do WordPress no rodapé 
+?>

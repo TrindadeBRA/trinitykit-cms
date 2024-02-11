2018a47a5e60d2c03c9b742166c0d310e2df5c56
Lucas Trindade <trindadebra@gmail.com>
2024-02-11 20:59:21 -0300

    Teste page

--- functions.php
+++ functions.php
@@ -38,3 +38,29 @@ add_action('admin_enqueue_scripts', 'enqueue_custom_admin_styles');
 
 // Desativar notificações de plugins desatualizados
 add_filter('pre_site_transient_update_plugins', '__return_null');
+
+
+
+
+
+// Adicione uma função para registrar a página no menu do painel de administração
+function registrar_pagina_administracao() {
+    add_menu_page(
+        'Minha Página de Administração', // Título da página
+        'Minha Página', // Título do menu
+        'manage_options', // Capacidade necessária para acessar esta página
+        'minha-pagina-admin', // Slug da página
+        'mostrar_pagina_admin', // Função de callback para exibir o conteúdo da página
+        'dashicons-admin-generic', // Ícone do menu (veja https://developer.wordpress.org/resource/dashicons/)
+        30 // Posição do menu
+    );
+}
+add_action('admin_menu', 'registrar_pagina_administracao');
+
+// Função de callback para exibir o conteúdo da página
+function mostrar_pagina_admin() {
+    echo '<div class="wrap">';
+    echo '<h1>Minha Página de Administração</h1>';
+    // Adicione aqui o conteúdo da sua página
+    echo '</div>';
+}

5b6c230de3a8b0900c6b33d47e297da371e5a304
Lucas Trindade <trindadebra@gmail.com>
2024-02-11 22:33:32 -0300

    test

--- js/custom-script.js
+++ js/custom-script.js
@@ -6,10 +6,27 @@ jQuery(document).ready(function($) {
         console.log("clicaque")
 
         // Faz uma requisição GET
-        $.get({
+        $.ajax({
             url: 'https://api.github.com/repos/TrindadeBRA/trinitykit/actions/runs?status=completed&per_page=1', // Substitua pela URL de sua API
             success: function(response) {
-                console.log(response); // Loga a resposta da requisição
+                // const lastRunId = response.workflow_runs[0].workflow_id;
+                const lastRunId = response.workflow_runs[0].id;
+                console.log(">>>", lastRunId); // Loga o ID retornado pela primeira requisição
+
+                // Faz uma requisição POST com o ID obtido
+                $.ajax({
+                    url: 'https://api.github.com/repos/TrindadeBRA/trinitykit/actions/runs/' + lastRunId + '/rerun',
+                    type: 'POST',
+                    headers: {
+                        'Authorization': 'Bearer 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'
+                    },
+                    success: function(response) {
+                        console.log("Reexecução iniciada com sucesso!"); // Loga a confirmação da reexecução
+                    },
+                    error: function(xhr, status, error) {
+                        console.error(status + ': ' + error); // Loga erros caso ocorram
+                    }
+                });
             },
             error: function(xhr, status, error) {
                 console.error(status + ': ' + error); // Loga erros caso ocorram

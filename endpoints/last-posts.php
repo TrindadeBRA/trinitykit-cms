3bc1644ecd2253cec8965cf05393ea307002029d
Lucas Trindade <trindadebra@gmail.com>
2024-02-12 01:36:36 -0300

    last post endpoint added

--- endpoints/last-posts.php
+++ endpoints/last-posts.php
@@ -1,26 +1,29 @@
-<?php
+add_action('rest_api_init', 'custom_register_last_posts_endpoint');
 
-function custom_theme_api_endpoint() {
+function custom_register_last_posts_endpoint() {
     register_rest_route('trinitykit/v1', '/last-posts', array(
         'methods' => 'GET',
-        'callback' => 'custom_get_last_three_posts',
+        'callback' => 'custom_get_last_posts',
     ));
 }
 
-function custom_get_last_three_posts() {
+function custom_get_last_posts() {
     $args = array(
-        'posts_per_page' => 3,
-        'post_status' => 'publish',
+        'posts_per_page' => 3, // número de posts a serem recuperados
+        'post_status' => 'publish', // apenas posts publicados
+        'orderby' => 'date', // ordenar por data
+        'order' => 'DESC', // em ordem decrescente
     );
 
     $posts_query = new WP_Query($args);
+
     $posts = array();
 
     if ($posts_query->have_posts()) {
         while ($posts_query->have_posts()) {
             $posts_query->the_post();
 
-            $post = array(
+            $post_data = array(
                 'id' => get_the_ID(),
                 'title' => get_the_title(),
                 'description' => get_the_excerpt(),
@@ -40,13 +43,12 @@ function custom_get_last_three_posts() {
                 ),
             );
 
-            $posts[] = $post;
+            $posts[] = $post_data;
         }
     }
 
+    // Restaura as informações de post globais
     wp_reset_postdata();
 
-    return rest_ensure_response($posts);
+    return $posts;
 }
-
-add_action('rest_api_init', 'custom_theme_api_endpoint');

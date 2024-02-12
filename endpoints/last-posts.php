daf1d1d27c9a519fca8ba4636562f68bb63dcfdc
Lucas Trindade <trindadebra@gmail.com>
2024-02-12 17:56:16 -0300

    Test endpoint

--- endpoints/last-posts.php
+++ endpoints/last-posts.php
@@ -1,42 +1,39 @@
 <?php
+/*
+Plugin Name: Últimos Posts Endpoint
+Description: Adiciona um endpoint personalizado para obter os últimos 3 posts publicados.
+*/
 
-add_action('rest_api_init', 'custom_register_last_posts_endpoint');
+// Adiciona o endpoint personalizado
+add_action('rest_api_init', 'register_last_posts_endpoint');
 
-function custom_register_last_posts_endpoint() {
+function register_last_posts_endpoint() {
     register_rest_route('trinitykit/v1', '/last-posts', array(
         'methods' => 'GET',
-        'callback' => 'custom_get_last_posts',
+        'callback' => 'get_last_posts',
     ));
 }
 
-function custom_get_last_posts() {
+// Função para obter os últimos 3 posts publicados
+function get_last_posts($request) {
     $args = array(
-        'posts_per_page' => 3, // número de posts a serem recuperados
-        'post_status' => 'publish', // apenas posts publicados
-        'orderby' => 'date', // ordenar por data
-        'order' => 'DESC', // em ordem decrescente
+        'post_type' => 'post',
+        'posts_per_page' => 3,
+        'orderby' => 'date',
+        'order' => 'DESC',
     );
 
-    $posts_query = new WP_Query($args);
+    $posts = get_posts($args);
 
-    $posts = array();
+    $response = array();
 
-    if ($posts_query->have_posts()) {
-        while ($posts_query->have_posts()) {
-            $posts_query->the_post();
-
-            $post_data = array(
-                'id' => get_the_ID(),
-                'title' => get_the_title(),
-                'href' => get_permalink(),
-            );
-
-            $posts[] = $post_data;
-        }
+    foreach ($posts as $post) {
+        $response[] = array(
+            'title' => $post->post_title,
+            'content' => $post->post_content,
+            'link' => get_permalink($post->ID),
+        );
     }
 
-    // Restaura as informações de post globais
-    wp_reset_postdata();
-
-    return $posts;
+    return new WP_REST_Response($response, 200);
 }

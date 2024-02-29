488e7217f0ba19abd93164063e2a8194f8af3022
Lucas Trindade <trindadebra@gmail.com>
2024-02-29 13:07:14 -0300

    update acf same id of acf export

--- preconfig.php
+++ preconfig.php
@@ -1,36 +1,5 @@
 <?php
 
-function create_initial_pages_and_import_acf() {
-    // Check if the pages and ACF settings have already been created/imported previously
-    $imported = get_option('pages_and_acf_imported');
-
-    // If the pages and ACF settings have already been imported, do nothing
-    if ($imported === 'imported') {
-        return;
-    }
-
-    // Create the initial pages
-    create_initial_pages();
-
-    // Import ACF settings from acf-export.json
-    $acf_json_path = get_template_directory() . '/acf-export/acf-export.json';
-    if (file_exists($acf_json_path)) {
-        $json_data = file_get_contents($acf_json_path);
-        if ($json_data !== false) {
-            $settings = json_decode($json_data, true);
-            if (function_exists('acf_add_local_field_group')) {
-                foreach ($settings as $setting) {
-                    acf_add_local_field_group($setting);
-                }
-            }
-        }
-    }
-
-    // Mark that the pages and ACF settings have been created/imported
-    update_option('pages_and_acf_imported', 'imported');
-}
-
-// Function to create initial pages
 function create_initial_pages() {
     // Check if the pages have already been created previously
     $pages_created = get_option('pages_created');
@@ -93,5 +62,5 @@ function create_page_once($title, $slug) {
     }
 }
 
-// Execute the creation of initial pages and ACF import
-create_initial_pages_and_import_acf();
+// Execute the creation of initial pages
+create_initial_pages();

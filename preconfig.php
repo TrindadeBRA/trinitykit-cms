7abb24c3696b1fbe9aa124743a1952840fec3c86
Lucas Trindade <trindadebra@gmail.com>
2024-02-29 13:20:05 -0300

    Create pages + import acfs

--- preconfig.php
+++ preconfig.php
@@ -34,6 +34,16 @@ function create_initial_pages() {
         create_page_once($page['title'], $page['slug']);
     }
 
+    if (function_exists("acf_add_local_field_group")) {
+        $acf_json_data = locate_template("/acf-export/acf-export.json");
+        $custom_fields = $acf_json_data ? json_decode(file_get_contents($acf_json_data), true) : array();
+
+        foreach ($custom_fields as $custom_field) {
+            acf_add_local_field_group($custom_field);
+        }
+    }
+
+
     // Mark that the pages have been created
     update_option('pages_created', 'created');
 }
